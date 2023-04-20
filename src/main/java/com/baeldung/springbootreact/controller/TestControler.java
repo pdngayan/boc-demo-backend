658774525ced8dccef5d5048366f2c9ea49809e0
Gayan <gayan.dissanayake@ncinga.net>
2023-04-20 15:34:24 +0530

    test 2

--- src/main/java/com/baeldung/springbootreact/controller/TestControler.java
+++ src/main/java/com/baeldung/springbootreact/controller/TestControler.java
@@ -4,5 +4,9 @@ public class TestControler {
     public static void main(String[] args) {
         System.out.println("hi");
         System.out.println("hi2");
+        System.out.println("test branch");
+        System.out.println("test branch");
+        System.out.println("test branch");
+        System.out.println("test branch");
     }
 }
