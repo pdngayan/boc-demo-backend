28f8150009755b6e777e5ef05a5e0a234a59994d
Gayan <gayan.dissanayake@ncinga.net>
2023-04-20 15:45:28 +0530

    Revert "test 2"
    This reverts commit 244e28f9

--- src/main/java/com/baeldung/springbootreact/controller/TestControler.java
+++ src/main/java/com/baeldung/springbootreact/controller/TestControler.java
@@ -3,5 +3,7 @@ package com.baeldung.springbootreact.controller;
 public class TestControler {
     public static void main(String[] args) {
         System.out.println("hi");
+        System.out.println("hi2");
+        System.out.println("Test BranchOnly");
     }
 }
