aa18f8c0a8c4ef21b3ef877f0b6c04f75ab33d28
djweer88 <djweer88@gmail.com>
2023-04-24 15:10:24 +0530

    dilan commit

---
+++ src/main/java/com/baeldung/springbootreact/domain/Dilan.java
@@ -0,0 +1,43 @@
+package com.baeldung.springbootreact.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "dilan")
+public class Dilan {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+
+    public Dilan() {
+    }
+
+
+
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+}
