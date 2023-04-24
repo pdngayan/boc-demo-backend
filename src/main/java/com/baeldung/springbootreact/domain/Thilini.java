92ad664621c4172a666f061b2cb560fa0fbf57e7
Thilini <nirmali@boc.lk>
2023-04-24 14:18:37 +0530

    new class - Thilini

---
+++ src/main/java/com/baeldung/springbootreact/domain/Thilini.java
@@ -0,0 +1,54 @@
+package com.baeldung.springbootreact.domain;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+public class Thilini {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+    private String email;
+
+    public Thilini() {
+    }
+
+    public Thilini(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public Thilini(Long id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
