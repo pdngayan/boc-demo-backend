61d87d0a7d90d7eee43a43fcce102bd8c5d943a8
lasitha84 <lasitha@boc.lk>
2023-04-24 15:09:43 +0530

    test4

--- src/main/java/com/baeldung/springbootreact/repository/ClientRepository.java
+++ src/main/java/com/baeldung/springbootreact/repository/ClientRepository.java
@@ -1,6 +1,5 @@
 package com.baeldung.springbootreact.repository;
 
-import com.baeldung.springbootreact.domain.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ClientRepository extends JpaRepository<Client, Long> {
