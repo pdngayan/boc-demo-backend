f45af0dd389b4b87b59ff9081e826a6b92761c52
vindhaya <vinudara@gmail.com>
2023-04-25 12:10:50 +0530

    Add vindhaya.java file

---
+++ src/main/java/com/baeldung/springbootreact/domain/Vindhaya.java
@@ -0,0 +1,56 @@
+package com.baeldung.springbootreact.domain;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "vindhaya")
+
+public class Vindhaya {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+    private String email;
+
+    public Vindhaya() {
+    }
+
+    public Vindhaya(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public Vindhaya(Long id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
