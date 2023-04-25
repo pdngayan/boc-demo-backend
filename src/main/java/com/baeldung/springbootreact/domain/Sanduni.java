b0d5aca808982395ef23bc54911c6dfa06f2cc20
SCvipulasena <chathurikavipulasena@gmail.com>
2023-04-25 11:36:27 +0530

    commit 1

---
+++ src/main/java/com/baeldung/springbootreact/domain/Sanduni.java
@@ -0,0 +1,68 @@
+package com.baeldung.springbootreact.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "sanduni")
+public class Sanduni {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+    private String email;
+
+
+
+    private integer teleno;
+
+    public Client() {
+    }
+
+    public Client(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public Client(Long id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public integer getTeleno() {
+        return teleno;
+    }
+
+    public void setTeleno(integer teleno) {
+        this.teleno = teleno;
+    }
+}
